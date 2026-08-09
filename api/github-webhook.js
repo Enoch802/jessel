a237fbafdf46fcf3bb5caacfeaffdd31b502ff61
Babayemi Enoch Marvelous <mbabayemi.2501784@stu.cu.edu.ng>
2026-08-09 15:30:41 +0100

    Change equality check to case-insensitive for repo

--- api/github-webhook.js
+++ api/github-webhook.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
   const { data: project } = await supabase
     .from('projects')
     .select('*')
-    .eq('github_repo', repoFullName)
+    .ilike('github_repo', repoFullName)
     .single();
 
   if (!project) {
